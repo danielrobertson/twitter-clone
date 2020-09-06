f3f9153dbb1c73d86d633537cdae531bcac3776d
Daniel Robertson <danielrobertson733@gmail.com>
2020-09-06 01:32:19 -0500

    fake users

--- components/tweet-feed.js
+++ components/tweet-feed.js
@@ -7,30 +7,32 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import { faRetweet } from "@fortawesome/free-solid-svg-icons";
 
-export default function TweetFeed() {
-  const [tweets, setTweets] = useState([
-    "They threw the ball! 🏈 #sports ",
-    "What year is it",
-    "Tweety mctweetface",
-  ]);
-
-  const tweetIcon = (icon) => {
-    return (
-      <div key={icon} className="pr-16">
-        <FontAwesomeIcon className="w-2 text-gray-800" icon={icon} />
-      </div>
-    );
-  };
-
+export default function TweetFeed({ tweets }) {
   return (
     <div>
-      {tweets.map((t) => (
-        <div key={t} className="border p-5">
-          {t}
-          <div className="flex pt-3">
-            {[faComment, faRetweet, faHeart, faShareSquare].map((i) =>
-              tweetIcon(i)
-            )}
+      {tweets.map((t, idx) => (
+        <div key={idx} className="flex border p-5">
+          <div className="pr-2">
+            <img
+              className="rounded-full"
+              src={t.user.avatar}
+              alt="picture"
+            ></img>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex">
+              <div className="p-1 font-semibold ">{t.user.name}</div>
+              <div className="p-1 text-gray-600">{t.user.username}</div>
+              <div className="p-1 text-gray-600">· {t.timestamp}</div>
+            </div>
+            <div className="p-1">{t.text}</div>
+            <div className="flex pt-3">
+              {[faComment, faRetweet, faHeart, faShareSquare].map((i, idx) => (
+                <div key={idx} className="pr-16">
+                  <FontAwesomeIcon className="w-2 text-gray-800" icon={i} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       ))}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import TweetFeed from "../components/tweet-feed";
 import WhatsHappening from "../components/whats-happening";
 import WhoToFollow from "../components/who-to-follow";
 
-export default function IndexPage() {
+export default function IndexPage({ tweets }) {
   return (
     <div className="container mx-auto flex flex-row h-screen">
       <div className="flex-grow-0">
@@ -13,7 +13,7 @@ export default function IndexPage() {
       <div className="flex-grow border">
         <h2 className="text-2xl font-bold p-3">Home</h2>
         <CreateTweet />
-        <TweetFeed />
+        <TweetFeed tweets={tweets} />
       </div>
       <div className="flex-grow-0">
         <WhatsHappening />
@@ -21,4 +21,42 @@ export default function IndexPage() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://randomuser.me/api/?results=3");
+  const users = await response.json();
+
+  const tweets = [
+    {
+      user: {
+        name: users.results[0].name.first,
+        username: `@${users.results[0].name.first}`,
+        avatar: users.results[0].picture.thumbnail,
+      },
+      text: "They threw the ball! 🏈 #sports ",
+      timestamp: "Just now",
+    },
+    {
+      user: {
+        name: users.results[1].name.first,
+        username: `@${users.results[1].name.first}`,
+        avatar: users.results[1].picture.thumbnail,
+      },
+      text: "What year is it",
+      timestamp: "20m",
+    },
+    {
+      user: {
+        name: users.results[2].name.first,
+        username: `@${users.results[2].name.first}`,
+        avatar: users.results[2].picture.thumbnail,
+      },
+      text: "Tweety mctweetface",
+      timestamp: "1h",
+    },
+  ];
+  return {
+    props: { tweets },
+  };
 }
