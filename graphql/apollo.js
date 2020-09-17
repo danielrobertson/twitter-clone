71271482209f096cf8ea7d6ddaae5a3efd807e6a
Daniel Robertson <danielrobertson733@gmail.com>
2020-09-16 20:32:45 -0500

    added typescript support

--- graphql/apollo.js
+++
@@ -1,7 +0,0 @@
-const { ApolloClient, InMemoryCache } = require("@apollo/client");
-
-export const apolloClient = new ApolloClient({
-  // todo env var this
-  uri: "https://selected-spaniel-87.hasura.app/v1/graphql",
-  cache: new InMemoryCache(),
-});
